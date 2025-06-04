c7f601403e94553af4633b9605b0653ac9a9a97b
João Leão <jv.cl2017@gmail.com>
2025-06-04 12:15:06 -0300

    Email Integrado com Recuperação de Senha

--- meu_site/settings.py
+++ meu_site/settings.py
@@ -77,7 +77,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Configurações de autenticação ...:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jv.cl2017@gmail.com'
+EMAIL_HOST_PASSWORD = 'pcwr ezca phsc gggo'
+
 
 # Idioma e horário
 LANGUAGE_CODE = 'pt-br'
